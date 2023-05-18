079a95bda2aa24b36350f91a573621a9902a82d1
rebeccaaaaOrram <rebecca.ignacio@orram.com.br>
2023-05-18 13:06:02 -0300

    feat: add logo in mobile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -170,14 +170,7 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
         icon={<FiMenu />}
       />
 
-      <Text
-        display={{ base: "flex", md: "none" }}
-        fontSize="2xl"
-        fontFamily="monospace"
-        fontWeight="bold"
-      >
-        Logo
-      </Text>
+      <Image src={Logo} alt="Osher Logo" />
 
       <HStack spacing={{ base: "0", md: "6" }}>
         <Flex alignItems={"center"}>
